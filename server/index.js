a0c6d6e394e9d7139440065b2481c22f5844b1d1
M4T <mateo.lesiuk@gmail.com>
2019-04-16 22:27:20 +0200

    Add, fetch user/user notes, add db file to ignored for git

--- server/index.js
+++ server/index.js
@@ -1,11 +1,73 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const low = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+
+const shortid = require('shortid');
+
+const adapter = new FileAsync('db.json');
 
 const app = express();
 
-app.get('/api', (req, res) => {
+const PORT = process.env.PORT || 3030;
+
+// Middleware
+app.use(bodyParser.json());
+// app.use(cors);
+
+// Echo/test route
+app.get('/echo', (req, res) => {
   res.json({ msg: 'API works' });
 });
 
-const PORT = process.env.PORT || 3030;
+low(adapter)
+  .then(db => {
+    // Routes
 
-app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+    // GET users notes
+    app.get('/api/:userId/notes', async (req, res) => {
+      const { userId } = req.params;
+      const hasUser = await db
+        .get('users')
+        .has(userId)
+        .value();
+
+      // If user does not exist, create it
+      if (!hasUser) {
+        await db
+          .get('users')
+          .assign({ [userId]: [] })
+          .write();
+      }
+
+      // Take users notes from db
+      const user = await db
+        .get('users')
+        .get(userId)
+        .value();
+
+      res.status(200).send(user);
+    });
+
+    // POST new note
+    app.post('/api/:userId/notes', async (req, res) => {
+      const { userId } = req.params;
+
+      const note = { id: shortid.generate(), ...req.body };
+
+      await db
+        .get('users')
+        .get(userId)
+        .push(note)
+        .write();
+
+      res.status(201).send(note);
+    });
+
+    // Set db default values
+    return db.defaults({ users: {} }).write();
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+  });
