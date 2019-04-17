e9d0e7837023117dccc8ae080ac2bcc15abd3475
M4T <mateo.lesiuk@gmail.com>
2019-04-17 08:20:24 +0200

    Add fetch note with id route/method, and update/patch note with id route/method

--- server/index.js
+++ server/index.js
@@ -27,42 +27,81 @@ low(adapter)
 
     // GET users notes
     app.get('/api/:userId/notes', async (req, res) => {
-      const { userId } = req.params;
-      const hasUser = await db
-        .get('users')
-        .has(userId)
-        .value();
-
-      // If user does not exist, create it
-      if (!hasUser) {
-        await db
+      try {
+        const { userId } = req.params;
+        const hasUser = await db
           .get('users')
-          .assign({ [userId]: [] })
-          .write();
+          .has(userId)
+          .value();
+
+        // If user does not exist, create it
+        if (!hasUser) {
+          await db
+            .get('users')
+            .assign({ [userId]: [] })
+            .write();
+        }
+
+        // Take users notes from db
+        const user = await db
+          .get('users')
+          .get(userId)
+          .value();
+
+        res.status(200).send(user);
+      } catch (err) {
+        console.log(err);
       }
-
-      // Take users notes from db
-      const user = await db
-        .get('users')
-        .get(userId)
-        .value();
-
-      res.status(200).send(user);
     });
 
     // POST new note
     app.post('/api/:userId/notes', async (req, res) => {
-      const { userId } = req.params;
+      try {
+        const { userId } = req.params;
 
-      const note = { id: shortid.generate(), ...req.body };
+        const note = { id: shortid.generate(), ...req.body };
 
-      await db
+        await db
+          .get('users')
+          .get(userId)
+          .push(note)
+          .write();
+
+        res.status(201).send(note);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // GET note by id
+    app.get('/api/:userId/notes/:noteId', async (req, res) => {
+      try {
+        const { userId, noteId } = await req.params;
+
+        const note = await db
+          .get('users')
+          .get(userId)
+          .find({ id: noteId })
+          .value();
+
+        res.status(200).send(note);
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    // PATCH note by id
+    app.patch('/api/:userId/notes/:noteId', async (req, res) => {
+      const { userId, noteId } = req.params;
+
+      const note = await db
         .get('users')
         .get(userId)
-        .push(note)
+        .find({ id: noteId })
+        .assign({ ...req.body })
         .write();
 
-      res.status(201).send(note);
+      res.status(200).send(note);
     });
 
     // Set db default values
